aad8c19c1aa158b4babd1f298a8e6450141c88fa
VeriBotOfficial <142978843+VeriBotOfficial@users.noreply.github.com>
2024-06-13 12:18:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let points = 0;
 
-// Load saved points from cookies
+// Load saved points from local storage
 window.onload = function() {
-    let savedPoints = getCookie("points");
+    let savedPoints = localStorage.getItem("points");
     if (savedPoints) {
         points = parseInt(savedPoints);
         document.getElementById("points").innerText = "Points: " + points;
@@ -16,34 +16,7 @@ function clickCookie() {
     saveGame(); // Save game after each click
 }
 
-// Function to save game
+// Function to save game using local storage
 function saveGame() {
-    setCookie("points", points, 365);
-}
-
-// Function to set cookie
-function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        let date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-// Function to get cookie
-function getCookie(name) {
-    let nameEQ = name + "=";
-    let ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1, c.length);
-        }
-        if (c.indexOf(nameEQ) === 0) {
-            return c.substring(nameEQ.length, c.length);
-        }
-    }
-    return null;
+    localStorage.setItem("points", points);
 }
